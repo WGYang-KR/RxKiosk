6203d7157b62da69968c7d9ebcffbc2e75b2add3
양원국(WG.Yang) <72122503+WGYang-KR@users.noreply.github.com>
2022-06-20 14:02:46 +0900

    FEAT: MenuItem Model 구현

--- RxKiosk/Model/MenuItem.swift
+++ RxKiosk/Model/MenuItem.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct MenuItem: Decodable {
+    var name: String
+    var price: Int
+}
+
+extension MenuItem: Equatable {
+    static func == (lhs: MenuItem, rhs: MenuItem) -> Bool {
+        return lhs.name == rhs.name && lhs.price == rhs.price
+    }
+}
