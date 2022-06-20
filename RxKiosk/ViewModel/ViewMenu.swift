2650fea5ac73c5ac064e1b31c6002be78c1e9c37
양원국(WG.Yang) <72122503+WGYang-KR@users.noreply.github.com>
2022-06-20 14:13:39 +0900

    FEAT: ViewMenu 구현

--- RxKiosk/ViewModel/ViewMenu.swift
+++ RxKiosk/ViewModel/ViewMenu.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+struct ViewMenu {
+    var name: String
+    var price: Int
+    var count: Int
+    
+    init(_ item: MenuItem) {
+        name = item.name
+        price = item.price
+        count = 0
+    }
+    
+    init(name: String, price: Int, count: Int) {
+        self.name = name
+        self.price = price
+        self.count = count
+    }
+    
+    func asMenuItem() -> MenuItem {
+        return MenuItem(name: name, price: price)
+    }
+}
+
+extension ViewMenu: Equatable {
+    static func == (lhs: ViewMenu, rhs: ViewMenu) -> Bool {
+        return lhs.name == rhs.name && lhs.price == rhs.price && lhs.count == rhs.count
+    }
+}
